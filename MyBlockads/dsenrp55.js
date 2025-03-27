d073f7543296cbbee1137603d35af0475f245fcf
Whisper <114045385+mist-whisper@users.noreply.github.com>
2025-03-27 08:50:39 +0800

    创建 dsenrp55.js

---
+++ MyBlockads/dsenrp55.js
@@ -0,0 +1,22 @@
+/*
+
+dsenrp55学习网站
+
+Scheme示例：
+SenPlayer://x-callback-url/play?url=
+mkvpipurl://
+
+*/
+
+var m3u8 = $request.url;
+console.log(m3u8);
+var sch = $persistentStore.read('Scheme')
+console.log(sch);
+if (sch == null) {
+    var oU = m3u8;
+} else {
+    var oU = sch + m3u8;
+}
+console.log(oU);
+$notification.post("", "", "点击通知观看", { 'openUrl': oU })
+$done({})
