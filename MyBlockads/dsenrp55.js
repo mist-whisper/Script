4586e706845bac655d01c2bb417eac480b4b97ad
Whisper <114045385+mist-whisper@users.noreply.github.com>
2025-03-29 11:20:31 +0800

    更新 dsenrp55.js

--- MyBlockads/dsenrp55.js
+++ MyBlockads/dsenrp55.js
@@ -9,14 +9,18 @@ mkvpipurl://
 */
 
 var m3u8 = $request.url;
-console.log(m3u8);
+//console.log(m3u8);
 var sch = $persistentStore.read('Scheme')
-console.log(sch);
-if (sch == null) {
-    var oU = m3u8;
-} else {
-    var oU = sch + m3u8;
+//console.log(sch);
+var site = $request.headers;
+//console.log(site);
+if (site["sec-fetch-site"] === "cross-site") {
+    if (sch == null) {
+        var oU = m3u8;
+    } else {
+        var oU = sch + m3u8;
+    }
+    console.log(oU);
+    $notification.post("", "", "点击通知跳转观看", { 'openUrl': oU })
 }
-console.log(oU);
-$notification.post("", "", "点击通知观看", { 'openUrl': oU })
 $done({})
