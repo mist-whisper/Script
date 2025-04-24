51e7932834760a204d68ce4e19d8c15e0d7fa4f5
wish <114045385+mist-whisper@users.noreply.github.com>
2025-04-24 15:03:11 +0800

    Update JD_Price.js

--- JD/JD_Price.js
+++ JD/JD_Price.js
@@ -181,15 +181,14 @@ body, table {
 </style>
 <script>
 const setTimeBasedTheme = () => {
-    if (isDark) {
-          rootElement.setAttribute('data-theme', 'dark');
-        } else {
-          rootElement.setAttribute('data-theme', 'light');
-        }
-      }
+    const themeTime = "${$.themeTime}".split("-");
+    let start = parseInt(themeTime[0]) || 7;
+    let end = parseInt(themeTime[1]) || 19;
+    const currentHour = new Date().getHours();
+    const isDarkTime = currentHour < start || currentHour >= end;
     document.documentElement.setAttribute('data-theme', isDarkTime ? 'dark' : 'light');
     console.log('Theme set to:', document.documentElement.getAttribute('data-theme'));
-;
+};
 
 // 图表初始化函数
 const initializeChart = () => {
