f431ef851fda3dfa04a0e5b184c937c6e6b106fe
wish <114045385+mist-whisper@users.noreply.github.com>
2025-05-09 18:30:12 +0800

    Update StreamMusic.js

--- StreamMusic/StreamMusic.js
+++ StreamMusic/StreamMusic.js
@@ -1,19 +1,5 @@
 // https://raw.githubusercontent.com/osinx/Script/main/vip/streamMusic.js
 
-/*
-#!name = 音流
-#!desc =  永久解锁VIP
-#!author = osinx
-#!homepage = https://github.com/osinx/Script
-#!icon = https://raw.githubusercontent.com/osinx/Script/main/icon/streamMusic.png
-
-[Script]
-http-response ^https:\/\/pay\.aqzscn\.cn\/api\/v1\/payments\/ script-path = https://raw.githubusercontent.com/osinx/Script/main/vip/streamMusic.js, requires-body = true, tag = 音流解锁VIP
-
-[Mitm]
-hostname = pay.aqzscn.cn
-*/
-
 let res = JSON.parse($response.body);
 
 const url = $request.url;
