decd9e6ab4119a729d3a4df700c8ff225a61acfe
wish <114045385+mist-whisper@users.noreply.github.com>
2025-05-21 23:17:19 +0800

    Update Amap_remove_ads.js

--- Amap/Amap_remove_ads.js
+++ Amap/Amap_remove_ads.js
@@ -1,5 +1,5 @@
 /*
-引用地址 https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/amap.js
+引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/amap.js
 */
 // 2025-03-31 09:45
 
