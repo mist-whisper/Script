3470f4d55c3c705600000df84723900c25908bbc
wish <114045385+mist-whisper@users.noreply.github.com>
2025-05-29 12:11:29 +0800

    Update header.js

--- NeteaseCloudMusic/header.js
+++ NeteaseCloudMusic/header.js
@@ -22,34 +22,9 @@ if (isNetEase) {
     header["mconfig-info"] = mconfig;
     header["user-agent"] = userAgent;
     
-    console.log("✅ 网易云音乐会员解锁成功🎉");
+    console.log("✅ 网易云音乐会员已解锁 🎉");
     $done({ headers: header });
   }
 } else {
   $done({});
 }
-
-
-/*
-const url = $request.url;
-const header = $request.headers;
-const isNetEase = url.includes("/interface") && url.includes(".music.163.com/");
-
-if (isNetEase) {
-  if (
-    $persistentStore.read("Music163_MConfigInfo") === undefined ||
-    $persistentStore.read("Music163_MConfigInfo") === null
-  ) {
-    $notification.post("网易云音乐遇到问题", "参数缺失", "请在插件内填入会员数据");
-    $done({});
-  } else {
-    header["cookie"] = $persistentStore.read("Music163_Cookie");
-    header["mconfig-info"] = $persistentStore.read("Music163_MConfigInfo");
-    header["user-agent"] = $persistentStore.read("Music163_UserAgent");
-  }
-
-  $done({ headers: header });
-} else {
-  $done({});
-}
-*/
