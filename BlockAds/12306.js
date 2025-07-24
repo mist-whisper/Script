2fae80376d9901bb49181dd9a990ea256b9cc52b
wish <114045385+mist-whisper@users.noreply.github.com>
2025-07-24 11:53:00 +0800

    12306.js aktualisieren

--- BlockAds/12306.js
+++ BlockAds/12306.js
@@ -1,54 +1,23 @@
-引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
+/*
+引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/12306.js
 */
-// 2024-11-24 18:15
+// 2024-02-19 10:45
 
-const url = $request.url;
-const header = $request.headers;
-const contype = header["content-type"];
-const headopt = header["operation-type"];
+let body = "";
+let obj = JSON.parse($request.body);
+const isQuanX = typeof $task !== "undefined";
 
-if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
-  // 12306页面内容
-  const list12306 = [
-    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
-    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
-    // "com.cars.otsmobile.city",
-    // "com.cars.otsmobile.initCountry",
-    // "com.cars.otsmobile.initNewSysCache",
-    // "com.cars.otsmobile.initProvince",
-    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
-    "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
-    // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
-    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
-    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
-    "com.cars.otsmobile.newHomePageBussData", // 商品信息流
-    // "com.cars.otsmobile.newHomePageRefresh",
-    "com.cars.otsmobile.paySuccBuss.bussEntryShow" // 商业推广
-    // "com.cars.otsmobile.travelPage.initData", // 出行服务
-  ];
-  if (list12306?.includes(headopt)) {
-    $done();
-  } else {
-    $done({});
-  }
-} else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
-  // 中国农业银行开屏广告
-  const listbankabc = [
-    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
-    "com.abchina.mbank.common.homepage.getStartParam"
-  ];
-  if (listbankabc?.includes(headopt)) {
-    $done();
-  } else {
-    $done({});
-  }
-} else if (url.includes("/sec.sginput.qq.com/q")) {
-  // 搜狗输入法候选词推广
-  if (contype === "application/octet-stream") {
-    $done();
-  } else {
-    $done({});
-  }
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[]}';
 } else {
-  $done({});
+  body = '{"code":"00","message":"无广告返回"}';
+}
+
+if (isQuanX) {
+  $done({ body });
+} else {
+  $done({ response: { body } });
 }
