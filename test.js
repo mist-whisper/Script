300cd8a611df0d4aa4497a7d7ecbbd2535a3738c
wish <114045385+mist-whisper@users.noreply.github.com>
2025-08-31 03:34:28 +0800

    更新 test.js

--- test.js
+++ test.js
@@ -1,35 +1,22 @@
-/*
- * Kuwo 波点小镇入口屏蔽
- * 解决 body 为字节数组的问题
- */
-
 let body = $response.body;
-
 try {
-  // 如果 body 是数组/字节，需要转成字符串
-  if (typeof body !== "string") {
-    body = String.fromCharCode.apply(null, body);
-  }
-
   let obj = JSON.parse(body);
 
   if (obj && obj.data) {
-    if (obj.data.town !== undefined) {
-      console.log("波点小镇原始值: " + obj.data.town);
-      obj.data.town = 0;
-      console.log("波点小镇已修改为: " + obj.data.town);
+    if (obj.data.downloadAdvert !== undefined) {
+      console.log("原始 downloadAdvert:", obj.data.downloadAdvert);
+      obj.data.downloadAdvert = 0;
+      console.log("已修改 downloadAdvert:", obj.data.downloadAdvert);
     }
-
-    if (obj.data.personalizeAdvert !== undefined) {
-      console.log("广告开关原始值: " + obj.data.personalizeAdvert);
-      obj.data.personalizeAdvert = 0;
-      console.log("广告开关已修改为: " + obj.data.personalizeAdvert);
+    if (obj.data.showAd !== undefined) {
+      console.log("原始 showAd:", obj.data.showAd);
+      obj.data.showAd = 0;
+      console.log("已修改 showAd:", obj.data.showAd);
     }
+    body = JSON.stringify(obj);
   }
-
-  body = JSON.stringify(obj);
 } catch (e) {
-  console.log("波点小镇脚本出错: " + e);
+  console.log("脚本出错:", e);
 }
 
 $done({ body });
