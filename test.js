65a6ab3096042466d0154ea9e41d44dccb9773be
wish <114045385+mist-whisper@users.noreply.github.com>
2025-09-12 10:33:13 +0800

    更新 test.js

--- test.js
+++ test.js
@@ -1,24 +1,18 @@
-{
-  "desc": "success",
-  "data": {
-    "places": [
-      {
-        "title": "我的钱包",
-        "iconImg": "https://m1.img.10010.com/resources/7188192A31B5AE06E41B64DA6D65A9B0/20241031/png/20241031151815.png",
-        "url": "https://m.client.10010.com/mobileService/openPlatform/openPlatLineNew.htm?to_url=https://epay.10010.com/ci-mps-st-web"
-      },
-      {
-        "title": "话费优惠",
-        "subtitleDesc": "元",
-        "subtitleNum": "8.8",
-        "subtitleType": "1",
-        "labelType": "0",
-        "labelDesc": "",
-        "url": "https://m.client.10010.com/mobileService/openPlatform/openPlatLineNew.htm?to_url=https://epay.10010.com/ci-mps-st-web/HFPreferential"
-      }
-    ],
-    "payShowFlag": true,
-    "redDataSize": 1
-  },
-  "code": "0000"
-}
+// Surge / Loon JavaScript 脚本示例
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 重构 data，将有效格子组合成数组
+obj.data.places = [];
+
+if (obj.data.fourPlace) obj.data.places.push(obj.data.fourPlace);
+if (obj.data.firstPlace) obj.data.places.push(obj.data.firstPlace);
+
+// 删除原来的单独字段
+delete obj.data.fourPlace;
+delete obj.data.firstPlace;
+delete obj.data.secendPlace;
+delete obj.data.thirdPlace;
+
+// 输出修改后的 JSON
+$done({body: JSON.stringify(obj)});
